0f3ff8db7b2dac212b47f3c45dcebf36aefcf1eb
toshokan <toshokan@shojigate.net>
2023-03-02 12:05:52 -0500

    emacs: only tangle changed org sources

--- emacs/.emacs.d/internals.el
+++ emacs/.emacs.d/internals.el
@@ -17,14 +17,26 @@
 (defvar tkn/conf-list (sort (file-expand-wildcards (tkn/emacs-d-filename "*.org")) 'string<)
   "A list of config files to be sourced.")
 
+(defun tkn/resolve-filename (filename)
+  "If FILENAME is a symlink, resolves it, otherwise evalutes to FILENAME"
+  (let ((file-dir (file-name-directory filename))
+	(symlink-dest (file-symlink-p filename)))
+    (if symlink-dest
+	(expand-file-name (concat file-dir symlink-dest))
+      filename)))
+
 (defun tkn/org-babel-load-file (file)
   "The original function only tangles if the source file is newer
 than the tangled file. This is not sensible if the source file is
 a symlink, since the symlink date does not change"
-  (let* ((tangled-file (concat (file-name-sans-extension file) ".el")))
-    (org-babel-tangle-file file tangled-file "emacs-lisp")
-    (load-file tangled-file)
-    (message "Loaded %s" tangled-file)))
+  (let ((target-file (tkn/resolve-filename file)))
+    (org-babel-load-file target-file)
+    (message "Loaded %s" file)))
+
+  ;; (let* ((tangled-file (concat (file-name-sans-extension file) ".el")))
+  ;;   (org-babel-tangle-file file tangled-file "emacs-lisp")
+  ;;   (load-file tangled-file)
+  ;;   (message "Loaded %s" tangled-file)))
 
 (defun tkn/load-configs ()
   "Load each config file referenced in `tkn/conf-list`. If it is
